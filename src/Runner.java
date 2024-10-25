e75a58814b24387432c6bebdac7f64c172782da4
BT_1N3_24 <sample@bths.edu>
2024-10-25 13:11:18 -0400

    third

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("My name is Michelle");
-        System.out.println("I love sleeping");
+        System.out.println("My name is Jintao");
+        System.out.println("I love gaming");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Do you like sleeping? (y/n): ");
+        String ans = scan.nextLine();
+        if(ans.equals("y")){
+            System.out.println("You should give yourself a pillow");
+        } else {
+            System.out.println("Why");
+        }
     }
 }
