19b19b0ddd021ac5859d70403dd2e2d7be7ffe33
BT_1N3_24 <sample@bths.edu>
2024-10-25 13:08:16 -0400

    second

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,7 @@
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("My name is Michelle");
+        System.out.println("I love sleeping");
     }
 }
